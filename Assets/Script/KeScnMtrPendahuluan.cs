496119e08d4498eeb66455eb0cb9670297bdc253
dzikrihf07 <dzikrihf@gmail.com>
2022-06-23 19:43:45 +0700

    Penambahan tampilan pendahuluan

---
+++ Assets/Script/KeScnMtrPendahuluan.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeScnMtrPendahuluan : MonoBehaviour
+{
+    public void btPindah () {
+        Application.LoadLevel("ScnMtrPendahuluan");
+    }
+}
