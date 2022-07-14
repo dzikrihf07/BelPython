1fc03ad82cc6bbcff56a6b83716cead8b8d3d00b
dzikrihf07 <dzikrihf@gmail.com>
2022-07-14 13:43:28 +0700

    Penambahan tampilan komentar

---
+++ Assets/Script/KeScnMtrKomentar.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeScnMtrKomentar : MonoBehaviour
+{
+    public void btPindah()
+    {
+        SceneManager.LoadScene("ScnMtrKomentar1");
+    }
+}
