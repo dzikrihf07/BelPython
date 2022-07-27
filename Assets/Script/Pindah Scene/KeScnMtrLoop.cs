3f379b4245ec0a23f602d654a046d31c69dc3dcc
dzikrihf07 <dzikrihf@gmail.com>
2022-07-27 23:20:36 +0700

    Penambahan materi loop

---
+++ Assets/Script/Pindah Scene/KeScnMtrLoop.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeScnMtrLoop : MonoBehaviour
+{
+    public void btPindah()
+    {
+        SceneManager.LoadScene("ScnMtrLoop");
+    }
+}
